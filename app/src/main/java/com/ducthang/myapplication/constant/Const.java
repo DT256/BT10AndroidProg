dad22510a64e4919981e574a31a3d512faf76a89
dt256 <buiducthang13022004@gmail.com>
2025-04-04 09:46:44 +0700

    commit

---
+++ app/src/main/java/com/ducthang/myapplication/constant/Const.java
@@ -0,0 +1,7 @@
+package com.ducthang.myapplication.constant;
+
+public class Const {
+    public static final String MY_USERNAME = "username";
+    public static final String MY_IMAGES = "avatar";
+}
+
